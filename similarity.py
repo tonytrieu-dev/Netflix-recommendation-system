a97beb69922194b288837dbf03f55f07f12c3e28
T0NES <ttrie003@ucr.edu>
2024-12-25 19:55:37 -0800

    "Renamed parameters for clarity"

--- similarity.py
+++ similarity.py
@@ -1,6 +1,6 @@
 class SimilarityCalculator:
     @staticmethod
-    def calculate_text_similarity(reference_description: str, current_description: str) -> float:
-        first_unique_wordset = set(reference_description.lower().split())
-        second_unique_wordset = set(current_description.lower().split())
+    def calculate_text_similarity(recently_watched_description: str, comparison_title_description: str) -> float:
+        first_unique_wordset = set(recently_watched_description.lower().split())
+        second_unique_wordset = set(comparison_title_description.lower().split())
         return len(first_unique_wordset & second_unique_wordset) / len(first_unique_wordset | second_unique_wordset)
