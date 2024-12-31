509111c6007bb7916774f9f1c49d0ff03a3cbaeb
T0NES <ttrie003@ucr.edu>
2024-12-30 21:03:12 -0800

    "Fixed import issue"

--- netflix_recommender/__init__.py
+++ netflix_recommender/__init__.py
@@ -1,6 +1,6 @@
 from .recommender import ContentRecommender
 from .hybrid_recommender import HybridRecommender
 from .data_manager import DataManager
-from .similarity import calculate_similarity
+from .similarity import SimilarityCalculator
 
 __version__ = '0.1.0'
